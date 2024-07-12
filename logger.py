c5255984dc3e4d34b37ae35b2c9296f5feff7072
rahul0715 <ajaychouhan26367@gmail.com>
2024-07-12 11:57:27 +0530

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,22 +1,17 @@
 import logging
 from logging.handlers import RotatingFileHandler
 
-# Configure the logging settings
 logging.basicConfig(
-    level=logging.ERROR,  # Set the default logging level to ERROR
-    format="%(asctime)s - %(levelname)s - %(message)s [%(filename)s:%(lineno)d]",  # Log message format
-    datefmt="%d-%b-%y %H:%M:%S",  # Date format in the log messages
+    level=logging.ERROR,
+    format=
+    "%(asctime)s - %(levelname)s - %(message)s [%(filename)s:%(lineno)d]",
+    datefmt="%d-%b-%y %H:%M:%S",
     handlers=[
-        RotatingFileHandler("Assist.txt", maxBytes=50000000, backupCount=10),  # Log to a file with rotation
-        logging.StreamHandler(),  # Log to the console
+        RotatingFileHandler("Assist.txt", maxBytes=50000000, backupCount=10),
+        logging.StreamHandler(),
     ],
 )
-
-# Set the logging level for the pyrogram library to WARNING to reduce log verbosity
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-# Create a logger instance
-logger = logging.getLogger()
 
-# Test logging to verify configuration
-logger.error("Logger is configured and ready.")
+logging = logging.getLogger()
